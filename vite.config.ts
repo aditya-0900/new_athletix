6fcab6bb074f36fc57ba29becdbc32a6c41d99a6
Aditya Sahu <aditya.kumarsahu2025@vitstudent.ac.in>
2026-03-03 01:03:43 +0530

    Multi-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -16,6 +16,16 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
+    },
+
+    // 🔥 ADD THIS
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          signup: path.resolve(__dirname, 'signup.html'),
+        },
+      },
     },
   };
 });
